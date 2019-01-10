a42408bf0bce827f50c1b04d521e0966d3cf4b8d
Rami Taleb <rtale089@uottawa.ca>
2019-01-10 10:26:01 -0500

    added mutation query to purchase product. added validation for product model

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -15,4 +15,21 @@ Types::MutationType = GraphQL::ObjectType.define do
       )
     }
   end
+
+  field :purchase_product, Types::ProductType do
+    argument :id, !types.ID
+    description 'Purchase a product (decreases the inventory count of product by 1)'
+
+    resolve ->(obj, args, ctx) {
+      prod = Product.find_by(id: args[:id])
+      count = prod.inventory_count
+      if count > 0
+        count -= 1
+        prod.inventory_count = count
+        prod.save
+      end
+      Product.find_by(id: args[:id])
+    }
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  validates :title,
+            presence: true,
+            uniqueness: { case_sensitive: false,
+                          message: 'Product name has already been listed!' }
+
+  validates :price,
+            presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :inventory_count,
+            presence: true, numericality: { only_integer: true,
+                                            greater_than: 0 }
 end
