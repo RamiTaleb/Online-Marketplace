3aca76321c0dacaa3167d3f1982487dce84e3969
Rami Taleb <rtale089@uottawa.ca>
2019-01-11 22:46:57 -0500

    adding images and some fixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,4 @@ if Rails.env.development?
   end
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'index#index'
 end
