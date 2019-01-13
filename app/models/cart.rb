52ec88e9fb73dd119d93a3c7579a32ce7ed23659
Rami Taleb <rtale089@uottawa.ca>
2019-01-12 22:50:14 -0500

    Added comments

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,4 @@
 class Cart < ApplicationRecord
+  # Cart -> Item is a 1 - * association
   has_many :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  # Has a foreign key for both these models
   belongs_to :product
   belongs_to :cart
 end
